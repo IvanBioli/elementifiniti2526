4cf6b9672cd6a7fdab8ff776428ac1eef16885a9
Ivan Bioli <91244037+IvanBioli@users.noreply.github.com>
2026-03-27 09:55:07 +0100

    Add packages fix

---
+++ esercitazioni/ex00/ex00_packages.jl
@@ -0,0 +1,18 @@
+# Author: Ivan Bioli (https://github.com/IvanBioli)
+
+import Pkg
+# Pkg.activate("elementifinitiunipv_pkg") # Uncomment this line if you want to activate the package environment
+Pkg.add([
+    "CairoMakie",
+    "Gmsh",
+    "Gridap",
+    "GridapGmsh",
+    "LaTeXStrings",
+    "Memoize",
+    "Meshes",
+    "PlotlyJS",
+    "Plots",
+    "Revise",
+    "SparseArrays",
+    "TriplotRecipes"
+])
